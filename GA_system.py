bf478ff5ce2662222872d6523165575557a2f2ca
wade <wade@dianhun.cn>
2017-06-20 10:48:10 +0800

    Merge branch 'master' of http://192.168.112.47:2000/wade/game_system

--- GA_system.py
+++ GA_system.py
@@ -477,4 +477,4 @@ def market_data():
         return jsonify(dates,game1,game2)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80 ,debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=5000 ,debug=True, threaded=True)
